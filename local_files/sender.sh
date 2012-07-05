6ea826224d60fb99067aa2740956fc25c01a444d
Artur Spengler <letztes@gmail.com>
2012-07-05 20:41:07 +0200

    Stop deleting 5-minute interval uploaded files
    All files are to be uploaded en block so afterwards they can
    be viewed en block.
    
    The 5-minute snapshots are uploaded as duplicates.

--- local_files/sender.sh
+++ local_files/sender.sh
@@ -9,7 +9,6 @@ if [ $# -lt 2 ]; then
     echo "not enough parameters. DATETIMESTRING and CURRENT_IMAGES_DIRECTORY needed."
     exit
 fi
-rm $CURRENT_IMAGES_DIRECTORY/success.txt
 echo "    uploading files"
 
 ftp -n -i <<ENDOFINPUT
@@ -18,16 +17,9 @@ user $USERNAME $PASSWORD
 binary
 lcd $CURRENT_IMAGES_DIRECTORY
 mput $DATETIMESTRING*".jpg"
-get success.txt
 close
 bye
 
 ENDOFINPUT
-
-if [ -f $CURRENT_IMAGES_DIRECTORY/success.txt ]
-then
-    rm $CURRENT_IMAGES_DIRECTORY/$DATETIMESTRING"_"*".jpg"
-    echo "    remove uploaded files..."
-fi
 
 echo "   done."
